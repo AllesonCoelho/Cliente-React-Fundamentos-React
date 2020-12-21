388821c72270ae6f4533ad693c7cc8aee6b8a38e
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-21 17:37:33 -0300

    Componente Card #02

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,38 @@ import ComParametro from './components/basicos/com_parametro'
 import Primeiro from './components/basicos/primeiro'
 import Fragmento from './components/basicos/fragmento'
 import Card from './components/layouts/card'
+import './app.css'
 
 
-export default (props) =>{
-    return(
-        <div>
+export default () => {
+    return (
+        <div className="App">
             <h1></h1>
 
-            <Card titulo="004 Exemplo de card">
-            <Aleatorio min={1} max={30}/>
-            </Card>
+            <div className="Cards">
+                <Card titulo="004 Aleaotorio">
+                    <Aleatorio min={1} max={30} />
+                </Card>
 
-            <Card titulo="#003 Fragmento">
-            <Fragmento/>
-            </Card>
+                <Card titulo="#003 Fragmento">
+                    <Fragmento />
+                </Card>
 
-            <Card titulo="#002 Com parametro">
-            <ComParametro
-            titulo="Situacao do aluno"
-            aluno="pedro"
-            nota={9.2}
-            />
-            </Card>
+                <Card titulo="#002 Com parametro">
+                    <ComParametro
+                        titulo="Situacao do aluno"
+                        aluno="pedro"
+                        nota={9.2}
+                    />
+                </Card>
 
-            <Card titulo="#001 Primeiro componente">
-            <Primeiro/>
-            </Card>
+                <Card titulo="#001 Primeiro componente">
+                    <Primeiro />
+                </Card>
+
+            </div>
+
+
         </div>
     )
 }
